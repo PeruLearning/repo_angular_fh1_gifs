992eaa3349efa14def62ff2b924881ede2b186fe
Edinson Aldaz <edinson.aldaz@gmail.com>
2024-06-04 15:04:45 -0500

    gifs service created

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private _tagsHistory: string[] = [];
+
+  constructor() { }
+
+  public get tagsHistory(): string[] {
+    return [...this._tagsHistory];
+  }
+
+  public searchTag(tag: string): void {
+    this._tagsHistory.unshift(tag);
+    console.log(this.tagsHistory);
+  }
+}
